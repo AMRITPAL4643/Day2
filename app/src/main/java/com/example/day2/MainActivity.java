a3f325b3bc4a85307142a371945cac8e5696cc10
admin <amritpalsingh3027@gmail.com>
2019-03-28 19:24:04 -0400

    Login design and code edit

--- app/src/main/java/com/example/day2/MainActivity.java
+++ app/src/main/java/com/example/day2/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.day2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+   private EditText edtEmail;
+   private EditText edtpassword;
+   private Button btnsignin;
+   private Button btnclear;
+   private TextView lblmessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtpassword = findViewById(R.id.edtpassword);
+        lblmessage = findViewById(R.id.lblmessage);
+        btnsignin = findViewById(R.id.btnsignin);
+        btnclear = findViewById(R.id.btnclear);
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String email = edtEmail.getText().toString();
+                Toast.makeText(MainActivity.this,email,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
